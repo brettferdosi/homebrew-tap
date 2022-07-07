5e3894c3c8a2b11f797d5c7243967058c287d8a5
Brett Gutstein <brett.gutstein@cst.cam.ac.uk>
2022-07-07 12:39:49 +0100

    rclone-cask: bump version to 1.58.1

--- Casks/rclone-cask.rb
+++ Casks/rclone-cask.rb
@@ -1,15 +1,15 @@
 cask "rclone-cask" do
-  version "1.57.0" # update sha256 hashes with version
+  version "1.58.1" # update sha256 hashes with version
 
   if Hardware::CPU.intel?
-    sha256 "c25cfe8c61da6da361940904511fcafb0f305e6eaa926f9871045de55e6861a4"
+    sha256 "03b104accc26d5aec14088c253ea5a6bba3263ae00fc403737cabceecad9eae9"
     binary "rclone-v#{version}-osx-amd64/rclone"
     manpage "rclone-v#{version}-osx-amd64/rclone.1"
     url "https://github.com/rclone/rclone/releases/download/v#{version}/rclone-v#{version}-osx-amd64.zip",
       verified: "github.com/rclone/rclone/"
   else
     # arm version untested
-    sha256 "62ba75131d011310d74fe68be4e8757fb0d8bc373ecbb4112ead7dd031545ef0"
+    sha256 "eb547bd0ef2037118a01003bed6cf00a1d6e6975b6f0a73cb811f882a3c3de72"
     binary "rclone-v#{version}-osx-arm64/rclone"
     manpage "rclone-v#{version}-osx-arm64/rclone.1"
     url "https://github.com/rclone/rclone/releases/download/v#{version}/rclone-v#{version}-osx-arm64.zip",
