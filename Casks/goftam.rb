3cfc37e9c219dc6f21120aeee2046801d37ae651
Brett Gutstein <brett@normative.cc>
2023-03-28 22:58:57 -0500

    goftam: new sha256

--- Casks/goftam.rb
+++ Casks/goftam.rb
@@ -1,6 +1,6 @@
 cask "goftam" do
   version "0.3"
-  sha256 "234774a2813b57d9656e21468dce319e6ac94229917db21ac0bbf72cd237ba8d"
+  sha256 "fc9868e6873f1a785f5989c9cdf82add8c9f03eaa3ea347da5991ee2f9234d89"
 
   url "https://github.com/brettferdosi/goftam/releases/download/v#{version}/goftam.app.zip"
   appcast "https://github.com/brettferdosi/goftam/releases.atom"
